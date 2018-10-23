7d98105a8b7045c298358b88b1b202fdfff2f484
DavidHieselmayr <43745537+DavidHieselmayr@users.noreply.github.com>
2018-10-23 22:21:42 +0200

    Add files via upload

---
+++ Übung 4/countdown3/app.js
@@ -0,0 +1,7 @@
+const countdown = require("./countdown");
+var helloWorld = function(){
+    console.log("Hallo Welt!");
+}
+countdown.setDecrementsPerSecond(3);
+countdown.setCountdown(helloWorld,10);
+
